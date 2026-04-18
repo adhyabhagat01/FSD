7cc9600135a13467ea5acc929e37c3c4e5ff4a50
adhyabhagat01 <adhyabhagat716@gmail.com>
2026-04-19 01:48:32 +0530

    Delete models directory

--- models/project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const projectSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    techstack: String
-});
-
-module.exports = mongoose.model("Project", projectSchema);
